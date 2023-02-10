f8f8fbd2d91d00ea7c5f9545941a672cc1a3a11c
DawsonH <ddwh2005@gmail.com>
2023-02-10 11:09:47 -0500

    added start menu and text file saving

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ os.system("cls")
 
 spots = []
 rows = 1
-
+menu = True
 while rows <= 5:
     spots.append("A" + str(rows))
     spots.append("B" + str(rows))
@@ -16,4 +16,25 @@ while rows <= 5:
     spots.append("E" + str(rows))
     rows += 1
 
-print(f"{spots[:5]} \n\n{spots[5:10]} \n\n{spots[10:15]} \n\n{spots[15:20]} \n\n{spots[20:25]} \n\n{spots[25:30]}")
+def print_brd():
+    print(f"{'|'.join(spots[:5])}\n{'|'.join(spots[5:10])}\n{'|'.join(spots[10:15])}\n{'|'.join(spots[15:20])}\n{'|'.join(spots[20:25])}\n{'|'.join(spots[25:30])}")
+
+with open('data.txt', 'w') as f:
+    f.write(f"{'|'.join(spots[:5])}\n{'|'.join(spots[5:10])}\n{'|'.join(spots[10:15])}\n{'|'.join(spots[15:20])}\n{'|'.join(spots[20:25])}\n{'|'.join(spots[25:30])}")
+
+print(f"Welcome To One Player Battle Ship \nIn This Game Your Goal Is To Battle Against A AI Ship As You Boath Try To Sink Eachothers Ship")
+
+while menu == True: 
+    cont = input(f"\nType In 'ready' Or 'quit' to Proceed:").lower() 
+    if cont == "ready":
+        print_brd()
+        menu = False
+    elif cont == "quit":
+        os.system("cls")
+        exit()
+    else:
+        print("ERROR PLEASE ENTER A VALID INPUT")
+
+def player_input():
+    value = input("Please Pick A Spot To Fire At:")
+    
