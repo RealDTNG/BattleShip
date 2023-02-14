6f54cf4817369751a6a5c902c430f20cc6d4ca20
DawsonH <ddwh2005@gmail.com>
2023-02-14 00:29:32 -0500

    -added ALOT comments - added ai ship thing -added run function - finished logic -bug fixed
    Signed-off-by: RealDTNG <ddwh2005@gmail.com>

--- main.py
+++ main.py
@@ -1,56 +1,109 @@
-# Dawson Hoyle 
+# Dawson Hoyle
 # Feb 9 2023
 # Made to Play Battle Ship alone
-
+# Import necessary libraries
 import os
 import random
 
+# Clear the console screen
 os.system("cls")
 
-spots = []
-rows = 1
+# Initialize variables
+hits = 0
+run = 34
+txts = []     # list to store board positions
+rows = 1       # counter for number of rows in board
 menu = True
-let_to_num = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4}
+let_to_num = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4}   # dictionary to map column letters to indices
 
+# Loop through rows to populate spots list with board positions
 while rows <= 5:
-    spots.append("A" + str(rows))
-    spots.append("B" + str(rows))
-    spots.append("C" + str(rows))
-    spots.append("D" + str(rows))
-    spots.append("E" + str(rows))
+    txts.append("A" + str(rows))
+    txts.append("B" + str(rows))
+    txts.append("C" + str(rows))
+    txts.append("D" + str(rows))
+    txts.append("E" + str(rows))
     rows += 1
 
-txts = spots
+spots = txts    # create a copy of the board
 
-# "print_brd" prints the board on use of method
-
+# Define a function to print the board
 def print_brd():
-    print(
-        f"   A |B |C |D |E  \n1 |{'|'.join(spots[:5])}\n2 |{'|'.join(spots[5:10])}\n3 |{'|'.join(spots[10:15])}\n4 |{'|'.join(spots[15:20])}\n5 |{'|'.join(spots[20:25])}\n{'|'.join(spots[25:30])}")
+    print(f"   A |B |C |D |E  \n1 |{'|'.join(txts[:5])}\n2 |{'|'.join(txts[5:10])}\n3 |{'|'.join(txts[10:15])}\n4 |{'|'.join(txts[15:20])}\n5 |{'|'.join(txts[20:25])}\n{'|'.join(txts[25:30])}")
 
+# Define a function to write the board to a file
 def txt_write():
     with open('data.txt', 'w') as f:
-        f.write(
-            f"{'|'.join(txts[:5])}\n{'|'.join(txts[5:10])}\n{'|'.join(txts[10:15])}\n{'|'.join(txts[15:20])}\n{'|'.join(txts[20:25])}\n{'|'.join(txts[25:30])}")
+        f.write(f"{'|'.join(txts[:5])}\n{'|'.join(txts[5:10])}\n{'|'.join(txts[10:15])}\n{'|'.join(txts[15:20])}\n{'|'.join(txts[20:25])}\n{'|'.join(txts[25:30])}")
 
+# Define a function to place AI ships on the board
 def ai_ship():
-    ship1()
-    
-    def ship1():
-        random_ship = f'{random.randrange(0, 24):02}'
-        ship1a = random_ship
-        if random_ship in '04,09,14,19,24':
-            extra_spot = random.randrange(0,3)
-            
-        
-print(
-    f"Welcome To One Player Battle Ship \nIn This Game Your Goal Is To Battle Against An AI Ship And Try To "
-    f"Sink Its Ship")
+    def place_ship(length):
+        # Pick a random starting position for the ship
+        start_pos = random.randint(0, 24)
 
+        # Check if there is enough space to place the ship
+        while start_pos + length > 25:
+            start_pos = random.randint(0, 24)
+
+        # Check if the selected positions overlap with any existing ships
+        for i in range(start_pos, start_pos+length):
+            if spots[i] == 'S ':
+                # If the positions overlap, start over and try again
+                return place_ship(length)
+
+        # If there is enough space and no overlap, place the ship on the board
+        for i in range(start_pos, start_pos+length):
+            spots[i] = 'S '
+
+    # Place the two ships
+    place_ship(2)
+    place_ship(2)
+
+# Print game instructions and menu
+print(f"Welcome To One Player Battle Ship \nIn This Game Your Goal Is To Battle Against An AI Ship And Try To Sink Its Ship")
+
+# "player_input" takes a txt input from the user to find the spot they want to
+def player_input():
+    global value_let, value_num, turns
+    turns = 0  # Initialize the number of turns to 0
+    value_let = input("Please Pick A Column To Fire At (Ex.. A,B,C...):").upper()  # Prompt the user for a column to fire at
+    while value_let not in 'ABCDE':  # If the column entered is not valid, prompt the user again until a valid column is entered
+        print('Please Enter A Valid Column')
+        value_let = input("Please Pick A Column To Fire At (Ex.. A,B,C...):").upper()
+    value_num = input("Please Pick A Row To Fire At (Ex.. 1,2,3...):")  # Prompt the user for a row to fire at
+    while value_num not in '12345':  # If the row entered is not valid, prompt the user again until a valid row is entered
+        print('Please Enter A Valid Row')
+        value_num = input("Please Pick A Row To Fire At (Ex.. 1,2,3...):")
+    turns += 1  # Increment the number of turns taken by the player
+    return int(value_num), let_to_num[value_let]  # Return the row and column as numeric values
+
+def fire():
+    global turns, hits  # Use the global variable "turns"
+    locate = let_to_num[value_let] + (int(value_num) - 1) * 5  # Convert the row and column to a single integer index
+    if spots[locate] == 'S ':  # If the player hits a ship
+        spots[locate] = 'X '  # Mark the spot as hit
+        txts[locate] = 'X '
+        print("You've hit an AI ship!")
+        hits += 1
+        if hits == 4:
+            print("YOU WIN!!")
+            exit()
+    elif spots[locate] not in 'XO':  # If the spot is not empty and has not been hit before, print an error message
+        spots[locate] = 'O'  # Mark the spot as missed
+        txts[locate] = 'O'
+        print("You've missed!")
+    else:
+        print("ERROR, PLEASE ENTER AN EMPTY SPOT")  # If the spot has already been hit, print an error message
+        player_input()  # Ask the player to enter a new spot
+    turns -= 1  # Decrement the number of turns taken by the player
+
+# Loop through menu options
 while menu:
     cont = input(f"\nType In 'ready' Or 'quit' to Proceed:").lower()
     if cont == "ready":
-        print_brd()
+        ai_ship()
+        run = 0
         menu = False
     elif cont == "quit":
         os.system("cls")
@@ -58,29 +111,8 @@ while menu:
     else:
         print("ERROR PLEASE ENTER A VALID INPUT")
 
-# "player_input" takes a txt input from the user to find the spot they want to
-def player_input():
-    global value_let, value_num, turns
-    turns = 0
-    value_let = input("Please Pick A Column To Fire At (Ex.. A,B,C...):").upper()
-    while value_let not in 'ABCDE':
-        print('Please Enter A Valid Column')
-        value_let = input("Please Pick A Column To Fire At (Ex.. A,B,C...):").upper()
-    value_num = input("Please Pick A Row To Fire At (Ex.. 1,2,3...):")
-    while value_num not in '12345':
-        print('Please Enter A Valid Row')
-        value_num = input("Please Pick A Row To Fire At (Ex.. 1,2,3...):")   
-    turns += 1
-    return int(value_num), let_to_num[value_let]
-
-def fire():
-    locate = 0
-    locate = value_let + (value_num*5-5)    
-    if spots[locate] == 'S':
-        spots[locate] = 'X'
-    elif spots[locate] not in 'XO':
-        spots[locate] = 'O'
-    else:
-        print("ERROR, PLEASE ENTER A EMPTY SPOT")
-        turns -= 1
-        player_input()
+while run <=10:
+    print_brd()
+    player_input()
+    fire()
+    run += 1
